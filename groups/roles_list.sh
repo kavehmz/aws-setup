2cd2244f0eea936e12cedcbc636121a7faa1bf6e
Kaveh Mousavi Zamani <kavehmz@gmail.com>
2017-01-15 18:01:05 +0000

    removing awc-cli parts

--- groups/roles_list.sh
+++
@@ -1,4 +0,0 @@
-GROUP_NAME=roles_list
-
-aws iam create-group --group-name $GROUP_NAME
-aws iam put-group-policy --group-name $GROUP_NAME --policy-document file://roles_list.json --policy-name roles_list
--- users/create.sh
+++
@@ -1,9 +0,0 @@
-USER_NAME=$1
-
-aws iam create-user --user-name $USER_NAME
-aws iam add-user-to-group --user-name $USER_NAME --group-name roles_list
-
-# create-login-profile.json is create usign the following command
-# aws iam create-login-profile --generate-cli-skeleton > create-login-profile.json 
-aws iam create-login-profile --cli-input-json file://create-login-profile.json
-
